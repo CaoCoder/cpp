962e0f8e4d94fb610a4492d4442a46db448d04b2
曹帅奇 <2466200050@qq.com>
2023-03-04 17:26:22 +0800

    简单的做题

--- zuo_ti/简单做题的代码/test.cpp
+++ zuo_ti/简单做题的代码/test.cpp
@@ -3,18 +3,15 @@ using namespace std;
 
 int main()
 {
-	//int s[3][4] = { {1,2,3},{4,5,6},{7,8,9} };
-	//int(*p)[4];
-	//p = s;
-	//cout << *(p + 1) << endl;
-	//cout << p;
+	//拷贝构造函数 用a拷贝一个b
+	Rectangle(Rectangle  obj)
+	{
+		h = obj.h;
+		w = obj.w;
+	}
+	//用a拷贝b
+	Rectangle b(a);
 
-	int a = 10;
-	int* p = &a;//一级指针,p里面存储的是整型变量a的地址。
-
-	int** pp = &p;//二级指针,pp里面存储的是指针变量p的地址。
-
-	cout << *p << endl;//通过p可以访问到a。
-	cout << *pp << endl;//通过*pp可以拿到p。
+	Rectangle obj(a);
 	return 0;
 }
