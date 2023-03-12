6b87c68e45f486880538458a4b1747c293182ffc
曹帅奇 <2466200050@qq.com>
2023-03-12 23:22:55 +0800

    封装

---
+++ package_map_set/package_map_set/Map.h
@@ -0,0 +1,15 @@
+//#pragma once
+//#include "RBTree.h"
+//#include <iostream>
+//using namespace std;
+//
+//namespace cao
+//{
+//	template<class K, class V>
+//	class map
+//	{
+//
+//	private:
+//		RBTree<K, pair<K, V>> _t;
+//	};
+//}
---
+++ package_map_set/package_map_set/test.cpp
@@ -0,0 +1,10 @@
+
+#include <iostream>
+using namespace std;
+#include "Set.h"
+
+int main()
+{
+	
+	cao::test_set1();
+}
