95df00d36e18b5606837815ac960e024ad184aeb
曹帅奇 <2466200050@qq.com>
2023-03-26 20:41:14 +0800

    简单测试

--- zuo_ti/简单做题的代码/test.cpp
+++ zuo_ti/简单做题的代码/test.cpp
@@ -1,63 +1,23 @@
-#include <iostream>
-using namespace std;
+#define _CRT_SECURE_NO_WARNINGS 1
 
-// Function to partially sort an array so that every 5 is immediately followed by a 6
+#include <stdio.h>
+#include <ctype.h>
 
-/*The first while loop iterates through the array, 
-and the second while loop swaps after it finds the element to swap.*/
-void fix56(int*& arr, int size)
+double Sign(double x);
+double Sign(double x)
 {
-    int temp = 0;
-    int i = 0;
-    while (i < size - 1)
-    {
-        if (arr[i] == 5 && arr[i + 1] != 6)
-        {
-            int j = i + 1;
-            while (j < size)
-            {
-                if (arr[j] == 6 && arr[j - 1] != 5)
-                {
-                    temp = arr[i + 1];
-                    arr[i + 1] = arr[j];
-                    arr[j] = temp;
-                    break;
-                }
-                j++;
-            }
-        }
-        i++;
-    }
-}
-
-
-// Function to print out an array
-// Loop through groups of numbers and output
-void printArray(int arr[], int size)
-{
-    int i = 0;
-    while (i < size)
-    {
-        cout << arr[i] << " ";
-        i++;
-    }
-    cout << endl;
+	if (x > 0)
+		return 1;
+	else if (x == 0)
+		return 0;
+	else
+		return -1;
 }
 
 int main()
 {
-    int arr1[] = { 1, 5, 1, 4, 6 };
-    int* pArr1 = arr1;//Change array name variable
-    fix56(pArr1, sizeof(arr1) / sizeof(arr1[0]));
-    printArray(arr1, sizeof(arr1) / sizeof(arr1[0]));
-
-    int arr2[] = { 1, 5, 1, 4, 4, 5, 1, 6, 6 };
-    int* pArr2 = arr2;
-    fix56(pArr2, sizeof(arr2) / sizeof(arr2[0]));
-    printArray(arr2, sizeof(arr2) / sizeof(arr2[0]));
-
-    int arr3[] = { 5, 2, 6, 2, 4 };
-    int* pArr3 = arr3;
-    fix56(pArr3, sizeof(arr3) / sizeof(arr3[0]));
-    printArray(arr3, sizeof(arr3) / sizeof(arr3[0]));
+	double a = 0;
+	scanf("%f", &a);
+	int b = Sign(a);
+	printf("%d", b);
 }
