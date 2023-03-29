d10d7c0752fd31173fcd817469108748b116d0f5
曹帅奇 <2466200050@qq.com>
2023-03-29 22:35:18 +0800

    最大公因数

--- zuo_ti/简单做题的代码/test.cpp
+++ zuo_ti/简单做题的代码/test.cpp
@@ -1,23 +1,25 @@
-#define _CRT_SECURE_NO_WARNINGS 1
 
+#define _CRT_SECURE_NO_WARNINGS 1//这一行是为scanf加的，用scanf都要加上这一行
+#define _CRT_SECURE_NO_WARNINGS 1//这一行是为scanf加的，用scanf都要加上这一行
 #include <stdio.h>
-#include <ctype.h>
-
-double Sign(double x);
-double Sign(double x)
-{
-	if (x > 0)
-		return 1;
-	else if (x == 0)
-		return 0;
-	else
-		return -1;
-}
-
 int main()
 {
-	double a = 0;
-	scanf("%f", &a);
-	int b = Sign(a);
-	printf("%d", b);
+	int a, b, c;
+	scanf("%d %d", &a, &b);
+	if (a > b) {
+		c = b;
+	}
+	else 
+	{ 
+		c = a;
+	}
+	for (int i = c; i > 1; i--)
+	{
+		if (a % i == 0 && b % i == 0) {
+			printf("%d", i);
+			break;
+		}
+	}
+
+	return 0;
 }
