933d84a2eccdcc90529d174851263ea747322c2f
曹帅奇 <2466200050@qq.com>
2023-04-03 23:29:58 +0800

    异常

---
+++ 异常/异常/test1.cpp
@@ -0,0 +1,16 @@
+﻿#define _CRT_SECURE_NO_WARNINGS 1
+#include<iostream>
+using namespace std;
+
+int main()
+{
+ 
+    char  b = 'a', c = 'A';
+    for (int i = 0; i < 6; i++)
+
+        if (i % 2) cout << (char)(i + b);
+    else cout << (char)(i + c);
+    
+       
+    return 0;
+}
