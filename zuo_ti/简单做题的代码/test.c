672cff68a17f0c5d7028682169d40a63ade9e504
曹帅奇 <2466200050@qq.com>
2023-04-10 21:44:58 +0800

    小端字节序

--- zuo_ti/简单做题的代码/test.c
+++ zuo_ti/简单做题的代码/test.c
@@ -1,33 +1,9 @@
 #define  _CRT_SECURE_NO_WARNINGS
-#include <stdlib.h>
+#include <stdio.h>
 #include <stdio.h>
 int main() {
-    long long int x, y;
-    long long int sum = 0;
-    scanf("%lld", &x);//ÓÐ¼¸×é
-    long long int* a = (long long int* )malloc(sizeof(int) * 100001 * 3);
+	long long a = 1, b = 2, c = 3,d = 0,e = 0,f = 0;
 
-    for (int i = 0; i < x * 3; i++)
-    {
-        scanf("%lld", &a[i]);
-    }
-    for (int i = 0; i < 3 * x; i++)
-    {
-        for (int j = i + 1; j < 3 * x; j++) {
-            if (a[j] > a[i]) {
-                y = a[i];
-                a[i] = a[j];
-                a[j] = y;
-            }
-        }
-    }
-    for (int i = 1; 2 * i <= 2 * x; i++)
-    {
-        sum = a[2 * i - 1] + sum;
-    }
-    printf("%lld", sum);
-
-    free(a);
-    a = NULL;
-    return 0;
+	printf("%d %d %d %d %d %d\n", a, b, c,d,e,f);
+	return 0;
 }
