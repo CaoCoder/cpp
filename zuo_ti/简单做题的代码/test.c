ce321a5f6ef76e1011a5d48ae5eb7d34a606d802
曹帅奇 <2466200050@qq.com>
2023-04-11 23:42:25 +0800

    选择题

--- zuo_ti/简单做题的代码/test.c
+++ zuo_ti/简单做题的代码/test.c
@@ -1,9 +1,35 @@
 #define  _CRT_SECURE_NO_WARNINGS
 #include <stdio.h>
 #include <stdio.h>
+int func(int x)
+{
+	int count = 0;
+	while (x)
+	{
+		count++;
+		x = x & (x - 1);
+	}
+	return count;
+}
 int main() {
-	long long a = 1, b = 2, c = 3,d = 0,e = 0,f = 0;
-
-	printf("%d %d %d %d %d %d\n", a, b, c,d,e,f);
+	
+	printf("%d", func(9999));
 	return 0;
 }
+
+
+//babc  daa
+//0010011100001111
+//0010011100001110
+//0 1 2 3 4 5 6 7 8 9
+//1 1 2 3 4 5 6 7 8 9
+//2 1 2 3 4 5 6 7 8 9
+//3 1 2 3 4 5 6 7 8 9
+//4 1 2 3 4 5 6 7 8 9
+//5 1 2 3 4 5 6 7 8 9
+//6 1 2 3 4 5 6 7 8 9
+//7 1 2 3 4 5 6 7 8 9
+//8 1 2 3 4 5 6 7 8 9
+//9 1 2 3 4 5 6 7 8 9
+
+
