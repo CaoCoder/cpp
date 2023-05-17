7f57af76e7f4eeb83789504b39d006ae3d79ff00
曹帅奇 <2466200050@qq.com>
2023-05-17 23:18:59 +0800

    ..

--- zuo_ti/简单做题的代码/test.c
+++ zuo_ti/简单做题的代码/test.c
@@ -1,61 +1,79 @@
-//#include <iostream>
-#include <string>
-#include<stdio.h>
-#define _CRT_SECURE_NO_WARNINGS 
 #include <stdio.h>
 #include <stdlib.h>
-#define max 100
+#include <string.h>
 
-typedef struct tnode {
-	char name[max];
-	int parent;
-	int son[3];
-}BT;
-void print(BT* T);
-void print(BT* T) {
-	for (int i = 0; i < 28; i++)
-	{
-		if (T != NULL)
-		{
-			printf("%s", T->name[i]);
+#define MAX_NODES 100
 
-		}
-	}
+typedef struct node {
+    char name[50];
+    int parent;
+    int children[3];
+} Node;
+
+void print(Node* nodes, int index, int level);
+void print_padding(int level);
+void print_vertical_line(int level);
+
+int main() {
+    Node nodes[MAX_NODES] = {
+        {"中华大学", -1, {1, 2, -1}},
+        {"计算机学院", 0, {3, 4, 5}},
+        {"电信学院", 0, {6, 7, -1}},
+        {"计算机科学", 1, {8, 9, 10}},
+        {"信息安全", 1, {11, 12, -1}},
+        {"物联网", 1, {13, -1, -1}},
+        {"计科1班", 3, {14, -1, -1}},
+        {"计科2班", 3, {15, -1, -1}},
+        {"计科3班", 3, {16, -1, -1}},
+        {"信安1班", 4, {17, -1, -1}},
+        {"信安2班", 4, {18, -1, -1}},
+        {"物联班", 5, {19, -1, -1}},
+        {"电子信息类", 2, {20, 21, 22}},
+        {"信息工程", 2, {23, 24, -1}},
+        {"电信1班", 6, {25, -1, -1}},
+        {"电信2班", 6, {26, -1, -1}},
+        {"电信3班", 6, {27, -1, -1}},
+        {"信息1班", 7, {28, -1, -1}},
+        {"信息2班", 7, {29, -1, -1}},
+        {"32", 8, {-1, -1, -1}},
+        {"35", 9, {-1, -1, -1}},
+        {"33", 10, {-1, -1, -1}},
+        {"36", 11, {-1, -1, -1}},
+        {"38", 12, {-1, -1, -1}},
+        {"38", 13, {-1, -1, -1}},
+        {"40", 20, {-1, -1, -1}},
+        {"38", 21, {-1, -1, -1}},
+        {"42", 22, {-1, -1, -1}},
+        {"38", 23, {-1, -1, -1}},
+        {"35", 24, {-1, -1, -1}},
+    };
+    print(nodes, 0, 0); // 根节点的索引为 0，初始缩进层级为 0
+
+    return 0;
+
 }
-	int main() {
-		BT data[max] = {
-		{"中华大学",-1,{1,2}},
-		{"计算机学院",0,{3,4,5}},
-		{"电信学院",0,{6,7}},
-		{"计算机科学",1,{8,9,10}},
-		{"信息安全",1,{11,12}},
-		{"物联网",1,{13}},
-		{"计科1班",3,{14}},
-		{"计科2班",3,{15}},
-		{"计科3班",3,{16}},
-		{"信安1班",4,{17}},
-		{"信安2班",4,{18}},
-		{"物联班",5,{19}},
-		{"电子信息类",2,{20,21,22}},
-		{"信息工程",2,{23,24}},
-		{"电信1班",6,{25}},
-		{"电信2班",6,{26}},
-		{"电信3班",6,{27}},
-		{"信息1班",7,{28}},
-		{"信息2班",7,{29}},
-		{"32",8,{NULL}},
-		{"35",9,{NULL}},
-		{"33",10,{NULL}},
-		{"36",11,{NULL}},
-		{"38",12,{NULL}},
-		{"38",13,{NULL}},
-		{"40",20,{NULL}},
-		{"38",21,{NULL}},
-		{"42",22,{NULL}},
-		{"38",23,{NULL}},
-		{"35",24,{NULL}},
-		};
-		print(data);
 
-	}
-	
+void print_padding(int level) {
+    for (int i = 0; i < level; i++) {
+        printf(" "); // 每一级缩进两个空格
+    }
+}
+
+void print_vertical_line(int level) {
+    for (int i = 0; i < level; i++) {
+        if (i == level - 1) {
+            printf("|--");
+        }
+        else {
+            printf(" "); // 每一级缩进三个空格
+        }
+    }
+}
+
+void print(Node* nodes, int index, int level) {
+    print_padding(level);
+    printf("%s\n", nodes[index].name); for (int i = 0; i < 3 && nodes[index].children[i] != -1; i++) {
+        print_vertical_line(level);
+        print(nodes, nodes[index].children[i], level + 1);
+    }
+}
--- zuo_ti/简单做题的代码/test.cpp
+++ zuo_ti/简单做题的代码/test.cpp
@@ -1,20 +1,60 @@
-﻿//#include <iostream>
-#include <string>
-#include<stdio.h>
+﻿////#include <iostream>
+//#include <string>
+//#include<stdio.h>
+//#define _CRT_SECURE_NO_WARNINGS 
+//#include <stdio.h>
 //#include <stdlib.h>
-//using namespace std;
-#define max 100
-typedef struct tnode
-{
-	char name[max];
-	int parent;
-	int son[3];
-
-}BT;
-
-int main()
-{
-	BT data[max];
-	data[0] = { "中华", -1, {1, 3} };
-
-}
+//#define max 100
+//
+//typedef struct tnode {
+//	char name[max];
+//	int parent;
+//	int son[3];
+//}BT;
+//void print(BT* T);
+//void print(BT* T) {
+//	for (int i = 0; i < 28; i++)
+//	{
+//		if (T != NULL)
+//		{
+//			printf("%s", T->name[i]);
+//
+//		}
+//	}
+//
+//	int main() {
+//		BT data[max] = {
+//		{"中华大学",-1,{1,2}},
+//		{"计算机学院",0,{3,4,5}},
+//		{"电信学院",0,{6,7}},
+//		{"计算机科学",1,{8,9,10}},
+//		{"信息安全",1,{11,12}},
+//		{"物联网",1,{13}},
+//		{"计科1班",3,{14}},
+//		{"计科2班",3,{15}},
+//		{"计科3班",3,{16}},
+//		{"信安1班",4,{17}},
+//		{"信安2班",4,{18}},
+//		{"物联班",5,{19}},
+//		{"电子信息类",2,{20,21,22}},
+//		{"信息工程",2,{23,24}},
+//		{"电信1班",6,{25}},
+//		{"电信2班",6,{26}},
+//		{"电信3班",6,{27}},
+//		{"信息1班",7,{28}},
+//		{"信息2班",7,{29}},
+//		{"32",8,{NULL}},
+//		{"35",9,{NULL}},
+//		{"33",10,{NULL}},
+//		{"36",11,{NULL}},
+//		{"38",12,{NULL}},
+//		{"38",13,{NULL}},
+//		{"40",20,{NULL}},
+//		{"38",21,{NULL}},
+//		{"42",22,{NULL}},
+//		{"38",23,{NULL}},
+//		{"35",24,{NULL}},
+//		};
+//		print(data);
+//
+//	}
